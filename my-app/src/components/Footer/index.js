63aaf1927a23e5cdad8bf07d28288d9cad79b157
Joan J <joanne.javillo@gmail.com>
2021-06-05 16:53:19 +0930

    added my old portfolio to convert to react, added components Navbar, footer, modified index.js, app.css abd app.js

---
+++ my-app/src/components/Footer/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Footer = () => {
+    return (
+        <footer className="footer navbar-fixed bottom" >
+            <div className="container">
+                <a href="https://github.com/joannejavillo" target="blank"><i className="fab fa-github-square"></i></a>
+                <a href="https://twitter.com/home" target="blank"><i className="fab fa-twitter"></i></a>
+                <a href="https://www.linkedin.com/in/jo-anne-javillo-b88103206/" target="blank"><i
+                    className="fab fa-linkedin"></i></a>
+                <span className="textalign-center">Made by JJavillo - Adelaide SA</span>
+                <p> &copy; 2021</p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
